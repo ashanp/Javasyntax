586f655a477097e7f0f9b8ea177a059b90631396
Ashan Perera <ashanp@gmail.com>
2023-10-30 08:42:50 +0400

    forth commit

--- src/course/test/lamda/TestLamda.java
+++ src/course/test/lamda/TestLamda.java
@@ -17,6 +17,14 @@ public class TestLamda {
 	
 }
 
+
+/*
+ * This interface is ment to be used as part of above lamda expression
+ * If I change this by adding another abstract method, the above lamda failes.
+ * Therefore, to ensure that this interface is not modified by anyother developer, we add the annotation @FunctionalInterface.
+ * It is only for operational purposes. Its not used for any compilation of code. Only to show error in the interface method itself.
+ */
+@FunctionalInterface
 interface IprintInterface{
 	void print(String x);
 }
